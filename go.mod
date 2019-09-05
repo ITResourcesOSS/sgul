f0e0ec09a0c57778316cc8845c96c635fa503f64
Luca Stasio <joshuagame@gmail.com>
2019-09-05 08:57:09 +0200

    GetPrincipal() returns Principal from the request context

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ITResourcesOSS/sgul
 go 1.12
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/fatih/structs v1.1.0
 	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/go-mach/gm-config v0.0.0-20190711141405-3918a3917e5e
@@ -13,5 +14,6 @@ require (
 	github.com/snowzach/rotatefilehook v0.0.0-20180327172521-2f64f265f58c
 	github.com/spf13/viper v1.4.0
 	github.com/x-cray/logrus-prefixed-formatter v0.5.2
+	golang.org/x/crypto v0.0.0-20190829043050-9756ffdc2472 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
---
+++ jwt.go
@@ -0,0 +1,73 @@
+package sgul
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"strings"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+// Principal defines the struct registered into the Context
+// representing the authenticated user information from the JWT Token.
+type Principal struct {
+	Username string
+	Role     string
+}
+
+type ctxKey int
+
+const ctxPrincipalKey ctxKey = iota
+
+// ErrPrincipalNotInContext is returned if there is no Principal in the request context.
+var ErrPrincipalNotInContext = errors.New("No Principal in request context")
+
+// JwtAuthenticator is the JWT authentication middleware.
+// https://github.com/ChrisTheShark/golang-jwt/blob/master/main.go
+func JwtAuthenticator() func(next http.Handler) http.Handler {
+	conf := GetConfiguration().API.Security
+	secret := []byte(conf.Jwt.Secret)
+
+	jwtAuthenticator := func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			authorization := r.Header.Get("Authorization")
+			trimmedAuth := strings.Fields(authorization)
+
+			// Trim out Bearer from Authorization Header
+			if authorization == "" || len(trimmedAuth) == 0 {
+				http.Error(w, "", http.StatusUnauthorized)
+				return
+			}
+
+			claims := jwt.MapClaims{}
+			_, err := jwt.ParseWithClaims(trimmedAuth[1], claims,
+				func(token *jwt.Token) (interface{}, error) {
+					return secret, nil
+				})
+			if err != nil {
+				http.Error(w, "", http.StatusUnauthorized)
+				return
+			}
+
+			principal := Principal{
+				Username: claims["sub"].(string),
+				Role:     claims["auth"].(string),
+			}
+
+			ctx := context.WithValue(r.Context(), ctxPrincipalKey, principal)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		}
+
+		return http.HandlerFunc(fn)
+	}
+	return jwtAuthenticator
+}
+
+// GetPrincipal return the user authenticated Princiapl information from the request context.
+func GetPrincipal(ctx context.Context) (Principal, error) {
+	if principal, ok := ctx.Value(ctxPrincipalKey).(Principal); ok {
+		return principal, nil
+	}
+	return Principal{}, ErrPrincipalNotInContext
+}
