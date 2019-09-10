8ef529c0cd25dc1fcecaa830880584dc4495a28d
Luca Stasio <joshuagame@gmail.com>
2019-09-10 10:02:26 +0200

    Pager and Route Pager

--- paging.go
+++ paging.go
@@ -22,34 +22,62 @@ const ctxPageKey ctxPKey = iota + 1
 // ErrPagerNotInContext is returned if there is no Pager in the request context.
 var ErrPagerNotInContext = errors.New("Pager info not in Context")
 
+func pager(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		p := r.URL.Query().Get("page")
+		s := r.URL.Query().Get("size")
+		if p != "" && s != "" {
+			var pVal int
+			var sVal int
+			var err error
+			pVal, err = strconv.Atoi(p)
+			if err != nil {
+				RenderError(w, NewHTTPError(err, http.StatusBadRequest, "Malformed 'page' param", middleware.GetReqID(r.Context())))
+				return
+			}
+			sVal, err = strconv.Atoi(s)
+			if err != nil {
+				RenderError(w, NewHTTPError(err, http.StatusBadRequest, "Malformed 'size' param", middleware.GetReqID(r.Context())))
+				return
+			}
+			page := Page{Page: pVal, Size: sVal}
+			ctx := context.WithValue(r.Context(), ctxPageKey, page)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	}
+}
+
 // Pager is the query paging middleware
 func Pager() func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
-		fn := func(w http.ResponseWriter, r *http.Request) {
-			p := r.URL.Query().Get("page")
-			s := r.URL.Query().Get("size")
-			if p != "" && s != "" {
-				var pVal int
-				var sVal int
-				var err error
-				pVal, err = strconv.Atoi(p)
-				if err != nil {
-					RenderError(w, NewHTTPError(err, http.StatusBadRequest, "Malformed 'page' param", middleware.GetReqID(r.Context())))
-					return
-				}
-				sVal, err = strconv.Atoi(s)
-				if err != nil {
-					RenderError(w, NewHTTPError(err, http.StatusBadRequest, "Malformed 'size' param", middleware.GetReqID(r.Context())))
-					return
-				}
-				page := Page{Page: pVal, Size: sVal}
-				ctx := context.WithValue(r.Context(), ctxPageKey, page)
-				next.ServeHTTP(w, r.WithContext(ctx))
-			} else {
-				next.ServeHTTP(w, r)
-			}
-		}
-		return http.HandlerFunc(fn)
+		return http.HandlerFunc(pager(next))
+		// fn := func(w http.ResponseWriter, r *http.Request) {
+		// 	p := r.URL.Query().Get("page")
+		// 	s := r.URL.Query().Get("size")
+		// 	if p != "" && s != "" {
+		// 		var pVal int
+		// 		var sVal int
+		// 		var err error
+		// 		pVal, err = strconv.Atoi(p)
+		// 		if err != nil {
+		// 			RenderError(w, NewHTTPError(err, http.StatusBadRequest, "Malformed 'page' param", middleware.GetReqID(r.Context())))
+		// 			return
+		// 		}
+		// 		sVal, err = strconv.Atoi(s)
+		// 		if err != nil {
+		// 			RenderError(w, NewHTTPError(err, http.StatusBadRequest, "Malformed 'size' param", middleware.GetReqID(r.Context())))
+		// 			return
+		// 		}
+		// 		page := Page{Page: pVal, Size: sVal}
+		// 		ctx := context.WithValue(r.Context(), ctxPageKey, page)
+		// 		next.ServeHTTP(w, r.WithContext(ctx))
+		// 	} else {
+		// 		next.ServeHTTP(w, r)
+		// 	}
+		// }
+		// return http.HandlerFunc(fn)
 	}
 }
 
@@ -60,4 +88,19 @@ func GetPage(ctx context.Context) (Page, error) {
 	}
 
 	return Page{}, ErrPagerNotInContext
+}
+
+/*
+// JWTRouteAuthorizer is the JWT authentication middleware to use on single route (a.e. Chi router get, post, ...).
+func JWTRouteAuthorizer(roles []string) func(next http.HandlerFunc) http.HandlerFunc {
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		return jwtAuthorize(roles, next)
+	}
+
+}
+*/
+
+// RoutePager .
+func RoutePager(next http.HandlerFunc) http.HandlerFunc {
+	return pager(next)
 }
