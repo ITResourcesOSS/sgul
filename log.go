419076b8097095ea3704e7d6c7546444ebcbead9
Luca Stasio <joshuagame@gmail.com>
2019-09-16 11:29:35 +0200

    GetLoggerByConf

--- log.go
+++ log.go
@@ -61,6 +61,46 @@ func GetLogger() *zap.Logger {
 	return logger
 }
 
+// GetLoggerByConf .
+func GetLoggerByConf(conf Log) *zap.Logger {
+	onceLogger.Do(func() {
+		//conf := GetConfiguration().Log
+		env := os.Getenv("ENV")
+		var writerSyncer zapcore.WriteSyncer
+		var encoder zapcore.Encoder
+
+		if (env == "prod" || env == "production") && !conf.Console {
+			writerSyncer = getLogWriter(conf)
+		} else {
+			writerSyncer = zapcore.NewMultiWriteSyncer(
+				zapcore.AddSync(os.Stdout),
+				getLogWriter(conf))
+		}
+
+		if conf.JSON {
+			encoder = zapcore.NewJSONEncoder(getEncoderConfig())
+		} else {
+			encoder = zapcore.NewConsoleEncoder(getEncoderConfig())
+		}
+
+		lgLvl := zapcore.InfoLevel
+		if err := (*zapcore.Level).UnmarshalText(&lgLvl, []byte(conf.Level)); err != nil {
+			lgLvl = zapcore.InfoLevel
+		}
+
+		core := zapcore.NewCore(encoder, writerSyncer, lgLvl)
+
+		if conf.Caller {
+			logger = zap.New(core, zap.AddCaller())
+		} else {
+			logger = zap.New(core)
+		}
+
+	})
+
+	return logger
+}
+
 func getEncoderConfig() zapcore.EncoderConfig {
 	encoderConfig := zap.NewProductionEncoderConfig()
 	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
